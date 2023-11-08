cab99525e15586c31494661e5f31e729c8c67ca3
Joseph <josephylee72@gmail.com>
2023-11-08 11:58:33 -0800

    rich cli stuff

---
+++ lib/richstyling.py
@@ -0,0 +1,32 @@
+from rich import print
+from rich import box
+from rich.text import Text
+from rich.panel import Panel
+from rich.table import Table
+from rich.console import Console
+
+def user_input(string):
+    text = Text(string)
+    text.stylize('green')
+    print(text)
+
+def menu(string):
+    text = Text(string)
+    text.stylize('cyan')
+    print(Panel.fit(text))
+
+def error(string):
+    text = Text(string)
+    text.stylize('magenta')
+    print(Panel.fit(text))
+
+def qualifier(string):
+    text = Text(string)
+    text.stylize('yellow')
+    print(Panel.fit(text))
+
+def listing(string):
+    pass
+
+def details(string):
+    pass
