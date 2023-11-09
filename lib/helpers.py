ec4be975888386334015812d7d1dd142c8215f5d
Joseph <josephylee72@gmail.com>
2023-11-08 19:42:08 -0800

    wednesday night

--- lib/helpers.py
+++ lib/helpers.py
@@ -228,22 +228,22 @@ def view_car_details(customer, salesman, car):
     pass
 
 def list_cars(customer, salesman, current_list):
-    # from classes.Car import Car
-    # table = Table(title="Here are your search results.")
+    from classes.Car import Car
+    table = Table(title="Here are your search results.")
 
-    # for key in current_list[0].__dict__.keys():
-    #     if key != '_owned':
-    #         table.add_column(snake_case_to_title_case(key))
-    # for car in current_list:
-    #     table.add_row(f"{car.vehicle_type}", f"{car.new}", f"{car.make}", f"{car.model}", f"{car.year}", f"{car.miles}", f"{car.fuel_type}", f"{car.color}", f"{car.transmission}", f"{car.price}", f"{car.owned}")
+    for key in current_list[0].__dict__.keys():
+        if key != '_owned':
+            table.add_column(snake_case_to_title_case(key))
+    for car in current_list:
+        table.add_row(f"{car.vehicle_type}", f"{car.new}", f"{car.make}", f"{car.model}", f"{car.year}", f"{car.miles}", f"{car.fuel_type}", f"{car.color}", f"{car.transmission}", f"{car.price}", f"{car.owned}")
 
-    # console = Console()
-    # console.print(table)
+    console = Console()
+    console.print(table)
 
-    # user_input("Please select a vehicle by Id.")
-    # selected_id = input()
+    user_input("Please select a vehicle by Id.")
+    selected_id = input()
 
-    # view_car_details(customer, salesman, Car.get_by('id',selected_id))
+    view_car_details(customer, salesman, Car.get_by('id',selected_id))
     pass
 
 #customer, salesman
@@ -257,12 +257,12 @@ def browse_cars(customer, salesman):
 
         if choice.lower() == "any":
             search_dict["price"] = ["any"]
-        elif isinstance(choice, str) and choice.lower() != 'any':
-            error("That is an invalid string.")
-            choose_max_price()
-        else:
+        elif choice.isdigit() and len(choice):
             price = int(choice)
             search_dict["price"] = [price]
+        else:
+            error("That is an invalid string.")
+            choose_max_price()
 
         list_cars(customer, salesman, Car.search_cars(search_dict))
 
